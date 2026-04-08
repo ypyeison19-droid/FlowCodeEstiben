a486ae5c404ec447b5afbeab3c4bb2ab935fb754
YeisonPino <ypyeison19@gmail.com>
2026-04-08 11:10:57 -0500

    chore: configuracion inicial del lproyecto Estiben FLow

---
+++ src/app.js
@@ -0,0 +1,15 @@
+export function saludar (nombre){
+return "Hola " + nombe + ", bienvenido a aplicacion de Estiben Flow!" ;
+}
+
+export function despedir (nombre) {
+return "Hatsa pronto," + nombre + "¡Estiben Flow!";
+}
+
+export function estadoSistema () {
+return {
+estado: "activo"
+version : "1.0.0"
+message : "Sistema funcionando correctamente"
+};
+}
---
+++ test/app.test.js
@@ -0,0 +1,27 @@
+import {saludar , despedir , estadoSistema} from "../src/app.js"
+
+function ejecutarPruebas() {
+let pasadas = 0;
+let fallidas = 0;
+
+const r1 = saludar ("Estiben")
+if (r1.includes("Estiben")){
+console.log("Test 1 Pasado: funcion saludar en Estiben es correcta")
+pasadas++;
+}else{
+console.log("Test 1 Fallido:" , r1); 
+fallidas++;
+}
+
+constante actualEstado = estadoSistema();
+if (actualEstado.estado === "activo"){
+console.log("Test 2 :Pasado: estadoSistema() funciona en Estiben");
+pasadas++;
+}else{
+console.log ("Test 2 fallido:", estado, "en Estiben");
+fallidas++;
+}
+console.log ("\nResultados: " + pasadas + " pasadas, " , + fallidas + "fallidas");
+if (fallidas>0) process.exit(1); "fallidas"
+}
+ejecutarPruebas(); "ejecutar"

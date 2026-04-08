2f8a3432019a8aca8f85b75201eaeaa151864dd7
YeisonPino <ypyeison19@gmail.com>
2026-04-08 11:47:19 -0500

    chore: configuracion inicial del lproyecto Estiben FLow

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,10 @@
-import { saludar, despedir, estadoSistema } from "../src/app.js"
+import { saludar, despedir, estadoSistema , sumar} from "../src/app.js"
 
 function ejecutarPruebas() {
     let pasadas = 0;
     let fallidas = 0;
 
+    //Test 1 : Saludar
     const r1 = saludar("Estiben")
     if (r1.includes("Estiben")) {
         console.log("Test 1 Pasado: funcion saludar en Estiben es correcta")
@@ -13,6 +14,7 @@ function ejecutarPruebas() {
         fallidas++;
     }
 
+    //Test 2 : estadoSistema
     const actualEstado = estadoSistema();
     if (actualEstado.estado === "activo") {
         console.log("Test 2 :Pasado: estadoSistema() funciona en Estiben");
@@ -21,6 +23,15 @@ function ejecutarPruebas() {
         console.log("Test 2 fallido:", estado, "en Estiben");
         fallidas++;
     }
+    // Test 3: Sumar
+  const resultadoSuma = sumar(5, 10);
+  if (resultadoSuma === 15) {
+    console.log("Test 3 Pasado: funcion sumar(5, 10) es 15");
+    pasadas++;
+  } else {
+    console.log("Test 3 Fallido: se esperaba 15 pero dio", resultadoSuma);
+    fallidas++;
+  }
     console.log("\nResultados: " + pasadas + " pasadas, ", + fallidas + "fallidas");
     if (fallidas > 0) process.exit(1); "fallidas"
 }
